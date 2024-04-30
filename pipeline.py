eb881bfb9fcb58eae280e0b99ae37f9c3f48c0b1
JoanMontes <joan.montes01@estudiant.upf.edu>
2024-04-30 13:20:34 +0200

    Create pipeline.py

---
+++ pipeline.py
@@ -0,0 +1,42 @@
+# Data Curation
+
+# source separation
+# svara segmentation
+
+
+
+# Feature extraction
+
+# state-of-the-art numerical melodic features
+
+# minimum pitch extraction
+# maximum pitch extraction
+# pitch range extraction
+# mean pitch extraction
+# average start pitch extraction
+# average end pitch extraction
+# number of change points
+
+# state-of-the-art context features
+
+# preceding svara
+# succeeding svara
+# precedent-succeeding svara
+# preceding direction
+# succeeding direction
+# preceding-succeeding direction
+
+
+
+# Modelling
+#sklearn
+
+
+
+# Evaluation
+#accuracy 
+
+
+
+# GUI
+#(qt design), (streamlit)
