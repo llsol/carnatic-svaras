3bfcf1d5a6c59bb66485e0caef2fc16e153212e6
JoanMontes <joan.montes01@estudiant.upf.edu>
2024-05-08 19:09:13 +0200

    svaras features (test1)

--- pipeline.py
+++ pipeline.py
@@ -2,8 +2,9 @@ import librosa
 import numpy as np
 import pandas as pd
 import os
-import seaborn
 import compiam
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 
 # Data Curation
@@ -17,64 +18,78 @@ import compiam
 # Feature extraction
 
 # create a header array for the dataframe
-headers = ['seg num',
+headers = ['id',
            'max pitch', 
            'min pitch', 
-           'pitch range', 
            'mean pitch', 
-           'avg start pitch', 
-           'avg end pitch', 
            'num change points', 
-           'prec svara', 
-           'succ svara', 
-           'prec+succ svara', 
-           'prec dir', 
-           'succ dir', 
-           'prec+succ dir']
+           'standard deviation',
+           'zero cross',
+           'loudness']
 
-# create a dataframe
-df_features = pd.DataFrame(columns = headers)
+svaras = ['sa', 'ri', 'pa', 'ni', 'ma', 'ga', 'dha']
 
-# accessing the audio files
-single_svara_path = os.path.join('single_svara')
-
-# iterate through the audio files to extract the current state-of-the-art selected pitch features
-for file in os.listdir(single_svara_path):
-    # load the audio file
-    y, sr = librosa.load(os.path.join(single_svara_path, file))
+for svara in svaras:
     
-    # extract the pitch
-    pitches, magnitudes = librosa.piptrack(y = y, sr = sr)
+
+    # create a dataframe
+    df_features = pd.DataFrame(columns = headers)
+
+    # accessing the audio files
+    single_svara_path = f'single_svara/{svara}'
+    id = 0
+
+
+    # iterate through the audio files to extract the current state-of-the-art selected pitch features
+    for file in os.listdir(single_svara_path):
+        # load the audio file
+        y, sr = librosa.load(os.path.join(single_svara_path, file))
+        # extract the pitch
+        pitches, magnitudes = librosa.piptrack(y = y, sr = sr)
+
+        # filtrem les frequencies valides:
+        valid_pitches = pitches[pitches>0]
+        
+
+        # extract the pitch features
+        max_pitch = np.max(valid_pitches)
+        min_pitch = np.min(valid_pitches)
+
+        pitch_range = max_pitch - min_pitch
+        mean_pitch = np.mean(valid_pitches)
+        avg_start_pitch = np.mean(valid_pitches[0])
+        avg_end_pitch = np.mean(valid_pitches[-1])
+
+        second_derivative = np.diff(np.diff(magnitudes))
+        num_change_points =  np.sum(second_derivative[:-1] * second_derivative[1:] < 0)
+        
+        loudness = np.sqrt(np.mean(magnitudes**2))
+
+        std = np.std(valid_pitches)
+
+        zero_cross = np.sum(np.diff(magnitudes) * magnitudes[:,:-1] < 0)
+        
+
+        # cuenta el numero de svaras que hay:
+        id += 1
+
+        
+        # append the features to the dataframe
+        df_features = df_features.append({'id': id,
+                                        'max pitch': max_pitch, 
+                                        'min pitch': min_pitch, 
+                                        'mean pitch': mean_pitch, 
+                                        'num change points': num_change_points,
+                                        'standard deviation': std,
+                                        'zero cross': zero_cross,
+                                        'loudness': loudness 
+                                        }, ignore_index = True)
+
+
+    df_features.to_csv(f'features/{svara}_features.csv',index=False)
     
-    # extract the pitch features
-    max_pitch = np.max(pitches)
-    min_pitch = np.min(pitches)
-    pitch_range = max_pitch - min_pitch
-    mean_pitch = np.mean(pitches)
-    avg_start_pitch = np.mean(pitches[0])
-    avg_end_pitch = np.mean(pitches[-1])
-    num_change_points = None
-
-
-    # split the audio file name to extract the segment number
-    seg_num = file.split('_')[1]
-    seg_num = seg_num.split('.')[0]
-
-
     
-    # append the features to the dataframe
-    df_features = df_features.append({'seg num': seg_num,
-                                      'max pitch': max_pitch, 
-                                      'min pitch': min_pitch, 
-                                      'pitch_range': pitch_range, 
-                                      'mean pitch': mean_pitch, 
-                                      'avg start pitch': avg_start_pitch, 
-                                      'avg end pitch': avg_end_pitch, 
-                                      'num change points': num_change_points, 
-                                      }, ignore_index = True)
-
 # state-of-the-art numerical melodic features
-
 
 # state-of-the-art context features
 
